075011d324509dde59c435f31a727f9489c3f343
insung <in27sung@gmail.com>
2025-02-12 21:04:43 +0900

    Initial setting

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -25,7 +25,25 @@ const navItems = [
 ];
 
 const Header: FC = () => {
-  return <div>Header</div>;
+  return <header className="fixed top-0 left-0 right-0 z-50 bg-stone-200">
+    <div className="container !max-w-full px-4">
+      <div className="flex items-center justify-between h-20 py-4">
+        <div>
+          <a href="/">
+            <span className="text-xl font-bold uppercase">Insung H</span>
+          </a>
+        </div>
+        <svg width="24" 
+             height="24" 
+             viewBox="0 0 24 24" 
+             fill="none" 
+             xmlns="http://www.w3.org/2000/svg">
+          <rect x="3" y="15" width="18" height="2" fill="currentColor" />
+          <rect x="3" y="7" width="18" height="2" fill="currentColor" />
+        </svg>
+      </div>
+    </div>
+  </header>;
 };
 
 export default Header;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,26 @@ const config: Config = {
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
-        extend: {},
+        screens: {
+            'sm': '375px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+        },
+        extend: {
+            fontFamily: {
+                sans: ['var(--font-archivo)'],
+                archivo: ['var(--font-archivo)'],
+            },
+            container: {
+                center: true,
+                padding: {
+                    DEFAULT: '1rem',
+                    md: '2rem',
+                    lg: '4rem',
+                },
+            },
+        },
     },
     plugins: [],
 };
