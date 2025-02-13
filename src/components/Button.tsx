2b6a01ed8cb9d69158cbb68de92b6d5dd0f69cfc
insung <in27sung@gmail.com>
2025-02-13 20:07:30 +0900

    Refactor Button component and update Hero and Header sections with responsive design

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,23 @@
-import { ButtonHTMLAttributes } from "react"
+import { ButtonHTMLAttributes, ReactNode } from "react"
+import { twMerge } from "tailwind-merge"
 
-const Button = (props: ButtonHTMLAttributes<HTMLButtonElement>) 
-=> {
-    const { className, children, ...rest } = props;
+const Button = (
+    props: {
+        variant: "primary" | "secondary" | "text";
+        iconAfter?: ReactNode
+    } & ButtonHTMLAttributes<HTMLButtonElement>) => {
+    const { className, children, variant, iconAfter, ...rest } = props;
     return (
-        <button className={className} {...rest}>
+        <button className={twMerge(
+            "h-11 px-6 rounded-xl border border-red-orange-500 uppercase inline-flex items-center gap-2",
+            variant === "primary" && "bg-red-orange-500 text-white",
+            variant === "secondary" && "",
+            variant === "text" && "h-auto px-0 border-transparent",
+            className
+        )}
+            {...rest}>
             <span>{children}</span>
+            {iconAfter && <span>{iconAfter}</span>}
         </button>
     )
 }
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -6,32 +6,42 @@ import Button from "@/components/Button";
 
 const Hero: FC = () => {
   return (
-  <section>
-    <div className="container !max-w-full">
-      <h1 className="text-5xl mt-40">
-        Crafting digital experiences through code and createive design
-      </h1>
-      <Button>
-        <span>View My Work</span>
-        <svg 
-          xmlns="http://www.w3.org/2000/svg" 
-          fill="none" 
-          viewBox="0 0 24 24" 
-          strokeWidth="1.5" 
-          stroke="currentColor" 
-          className="size-6">
-          <path 
-            strokeLinecap="round" 
-            strokeLinejoin="round" 
-            d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5" />
-        </svg>
-      </Button>
-      <Button>Let&apos;s Talk</Button>
-    </div>
-    <div>
-      <Image src={heroImage} alt="My portrait" />
-    </div>
-  </section>
+    <section>
+      <div className="grid md:grid-cols-12">
+        <div className="md:col-span-7">
+          <div className="container !max-w-full">
+            <h1 className="text-5xl md:text-6xl mt-40">
+              Crafting digital experiences through code and createive design
+            </h1>
+            <div className="flex flex-col mt-10 items-start gap-6">
+              <Button
+                variant="secondary"
+                iconAfter={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-5">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5" />
+                  </svg>}>
+                <span>View My Work</span>
+              </Button>
+              <Button variant="text">Let&apos;s Talk</Button>
+            </div>
+          </div>
+        </div>
+        <div className="md:col-span-5">
+          <div className="mt-20">
+            <Image src={heroImage} alt="My portrait" />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
