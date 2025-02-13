03fbccf601f2a9724315b9001df8c707370292b4
insung <in27sung@gmail.com>
2025-02-14 08:58:56 +0900

    Add Intro and Projects sections with responsive design and content

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -36,7 +36,7 @@ const Hero: FC = () => {
           </div>
         </div>
         <div className="md:col-span-5">
-          <div className="mt-20 md:mt-0 h-full">
+          <div className="mt-20 md:mt-0 md:h-full">
             <Image src={heroImage} alt="My portrait" className="size-full object-cover" />
           </div>
         </div>
