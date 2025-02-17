ff06772d900654fdd3b9aba2bf2df226d3efc2ad
insung <in27sung@gmail.com>
2025-02-17 09:02:32 +0900

    Add Footer section with navigation and contact details

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,7 +1,57 @@
 import { FC } from "react";
+import Button from "@/components/Button";
+
+const navItems = [
+  {
+    href: '#',
+    label: 'Home'
+  },
+  {
+    href: '#',
+    label: 'Projects'
+  },
+  {
+    href: '#',
+    label: 'Testimonials'
+  },
+  {
+    href: '#',
+    label: 'Faqs'
+  },
+  {
+    href: '#',
+    label: 'Contact'
+  }
+]
 
 const Footer: FC = () => {
-  return <div>Footer</div>;
+  return <footer className="bg-stone-900 text-white">
+    <div className="container">
+      <div className="section">
+        <div className="flex items-center"></div>
+        <div className="size-3 rounded-full bg-green-400">
+          <span className="uppercase">One spot available for next month</span>
+        </div>
+        <h2 className="text-4xl mt-8 font-extralight">
+          Enough talk. Let's make someting great together.
+        </h2>
+        <Button variant="secondary" className="mt-8" iconAfter={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+        </svg>
+        }>in27sung@gamil.com</Button>
+        <nav className="flex flex-col gap-8 mt-16">
+          {navItems.map(({ href, label }) => (
+            <a href={href} key={label}>
+              {label}
+            </a>
+          ))}
+        </nav>
+      </div>
+      <p className="py-16">
+        Copyright &copy; Insung Hwang &bull; All rights reserved
+      </p>
+    </div>
+  </footer>;
 };
 
 export default Footer;
