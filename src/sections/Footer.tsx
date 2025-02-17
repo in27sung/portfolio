457cff1b535f9c194989d2bb7392e97610c2b6c9
insung <in27sung@gmail.com>
2025-02-17 09:40:21 +0900

    Enhance Footer layout and styling for improved responsiveness

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -28,26 +28,31 @@ const Footer: FC = () => {
   return <footer className="bg-stone-900 text-white">
     <div className="container">
       <div className="section">
-        <div className="flex items-center"></div>
-        <div className="size-3 rounded-full bg-green-400">
+        <div className="flex items-center gap-3">
+        <div className="size-3 rounded-full bg-green-400"></div>
           <span className="uppercase">One spot available for next month</span>
         </div>
-        <h2 className="text-4xl mt-8 font-extralight">
-          Enough talk. Let's make someting great together.
-        </h2>
-        <Button variant="secondary" className="mt-8" iconAfter={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-        </svg>
-        }>in27sung@gamil.com</Button>
-        <nav className="flex flex-col gap-8 mt-16">
-          {navItems.map(({ href, label }) => (
-            <a href={href} key={label}>
-              {label}
-            </a>
-          ))}
-        </nav>
+        <div className="grid md:grid-cols-3 md:items-center">
+          <div className="md:col-span-2">
+          <h2 className="text-4xl md:text-7xl mt-8 font-extralight">
+            Enough talk. Let's make someting great together.
+          </h2>
+          <Button variant="secondary" className="mt-8" iconAfter={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+          </svg>
+          }>in27sung@gamil.com
+          </Button>
+          </div>
+          <nav className="flex flex-col md:items-end gap-8 mt-16 md:mt-0">
+            {navItems.map(({ href, label }) => (
+              <a href={href} key={label}>
+                <Button variant="text" className="text-lg">{label}</Button>
+              </a>
+            ))}
+          </nav>
+        </div>
       </div>
-      <p className="py-16">
+      <p className="py-16 text-white/30 text-sm">
         Copyright &copy; Insung Hwang &bull; All rights reserved
       </p>
     </div>
