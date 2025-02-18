e406d227d16b70e5d907f3d6ab306354c7515c1b
insung <in27sung@gmail.com>
2025-02-19 08:44:45 +0900

    Update Intro.tsx

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -1,13 +1,46 @@
-import { FC } from "react";
+"use client";
+
+import { stagger, useAnimate, useInView } from "motion/react";
+import { FC, useEffect } from "react";
+import SplitType from "split-type";
 
 const Intro: FC = () => {
-  return <section className="section mt-12 md:mt-16 lg:mt-20" id="intro">
-    <div className="container max-w-screen-xl"> 
-      <h2 className="text-4xl md:text-7xl lg:text-8xl lg:w-[80%]">
-        Building beautiful websites with clean code and thoughtful design to help your business grow and stand out online
-      </h2>
-    </div>
-  </section>;
+  const [scope, animate] = useAnimate();
+  const inView = useInView(scope, {
+    once: true,
+  });
+
+  useEffect(() => {
+    new SplitType(scope.current.querySelector('h2'), {
+      types: 'lines,words',
+      tagName: "span",
+    });
+  }, [scope]);
+
+  useEffect(() => {
+    if (inView) {
+      animate(
+        scope.current.querySelectorAll('.word'),
+        {
+          transform: "translateY(0%)",
+        },
+        {
+          duration: 0.5,
+          delay: stagger(0.2),
+        }
+      );
+    }
+  }, [inView, animate, scope]);
+
+  return (
+    <section className="section mt-12 md:mt-16 lg:mt-20" id="intro" ref={scope}>
+      <div className="container max-w-screen-xl">
+        <h2 className="text-4xl md:text-7xl lg:text-8xl lg:w-[80%]">
+          Building beautiful websites with clean code and thoughtful design to help your business grow and stand out online
+        </h2>
+      </div>
+    </section>
+  );
 };
 
 export default Intro;
