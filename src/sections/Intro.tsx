cbc6b568b0e11841511c8a11ce76fad7cac0db9e
insung <in27sung@gmail.com>
2025-02-18 09:06:34 +0900

    Add section IDs for smooth scrolling and navigation

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 
 const Intro: FC = () => {
-  return <section className="section mt-12 md:mt-16 lg:mt-20">
+  return <section className="section mt-12 md:mt-16 lg:mt-20" id="intro">
     <div className="container max-w-screen-xl"> 
       <h2 className="text-4xl md:text-7xl lg:text-8xl lg:w-[80%]">
         Building beautiful websites with clean code and thoughtful design to help your business grow and stand out online
